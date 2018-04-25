a227a7371deade8bc916434738a0c314a85aa167
Leo Mehlig <leo@leomehlig.com>
2018-04-25 17:49:44 +0200

    Inital commit

---
+++ TDJSON/TDJSON.h
@@ -0,0 +1,19 @@
+//
+//  TDJSON.h
+//  TDJSON
+//
+//  Created by Leo Mehlig on 25.04.18.
+//  Copyright © 2018 Leo Mehlig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDJSON.
+FOUNDATION_EXPORT double TDJSONVersionNumber;
+
+//! Project version string for TDJSON.
+FOUNDATION_EXPORT const unsigned char TDJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDJSON/PublicHeader.h>
+
+
