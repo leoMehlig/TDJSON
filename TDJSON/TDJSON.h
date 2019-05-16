a65a7e0bcab28d57b4929fc2e5ac1f7f0c137190
Leo Mehlig <leo@leomehlig.com>
2019-05-16 12:39:56 +0200

    Fix carthage build

--- TDJSON/TDJSON.h
+++ TDJSON/TDJSON.h
@@ -1,10 +1,10 @@
-//#import <UIKit/UIKit.h>
+//#import <Foundation/Foundation.h>
 
 //! Project version number for TDJSON.
-FOUNDATION_EXPORT double TDJSONVersionNumber;
+//FOUNDATION_EXPORT double TDJSONVersionNumber;
 
 //! Project version string for TDJSON.
-FOUNDATION_EXPORT const unsigned char TDJSONVersionString[];
+//FOUNDATION_EXPORT const unsigned char TDJSONVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TDJSON/PublicHeader.h>
 
